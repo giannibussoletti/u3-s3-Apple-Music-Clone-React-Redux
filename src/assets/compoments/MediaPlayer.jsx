c4e188cd2d879c5071e613deb208b3c166455306
giannibussoletti <bussolettigianni@gmail.com>
2026-05-18 01:06:15 +0200

    timeline player function done

--- src/assets/compoments/MediaPlayer.jsx
+++ src/assets/compoments/MediaPlayer.jsx
@@ -1,25 +1,61 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import { Container, Row, Col, Image } from "react-bootstrap"
+import { Container, Row, Col, Image, Form } from "react-bootstrap"
 import { useSelector, useDispatch } from "react-redux"
 
 import { pauseSongAction, playPromiseAction, playSongAction } from "../redux/action"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const MediaPlayer = () => {
   const dispatch = useDispatch()
   const song = useSelector((reduxStore) => reduxStore.music.song)
-  const isPlaying = useSelector((reduxStore) => reduxStore.music.isPlaying)
+  const isPlaying = useSelector((reduxStore) => reduxStore.music.isPlaying) || false
   const audioPlayer = useRef(null)
-  const audio = audioPlayer.current
+
+  const [audio, setAudio] = useState(null)
+  const [songTime, setSongTime] = useState("00:00")
+  const [valueRange, setValueRange] = useState(0)
+  const [songTimeTotal, setSongTimeTotal] = useState("00:00")
 
   useEffect(() => {
+    setAudio(audioPlayer.current)
     if (isPlaying) {
       playPromiseAction(audio, song.preview)
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [song])
+
+  useEffect(() => {
+    if (isPlaying) {
+      const updateTime = () => {
+        const minutes = Math.floor(audio.currentTime / 60)
+        const seconds = Math.floor(audio.currentTime % 60)
+        const currSeconds = seconds < 10 ? "0" + seconds : seconds
+        const currMin = seconds < 10 ? "0" + minutes : minutes
+        setSongTime(currMin + ":" + currSeconds)
+        setValueRange(audio.currentTime)
+      }
+      audio.addEventListener("timeupdate", updateTime)
+
+      return () => audio.removeEventListener("timeupdate", updateTime)
+    }
+  }, [audio, isPlaying])
+
+  useEffect(() => {
+    if (isPlaying) {
+      const getTotalTime = () => {
+        const minutes = Math.floor(audio.duration / 60)
+        const seconds = Math.floor(audio.duration % 60)
+        const currSeconds = seconds < 10 ? "0" + seconds : seconds
+        const currMin = seconds < 10 ? "0" + minutes : minutes
+        setSongTimeTotal(currMin + ":" + currSeconds)
+      }
+      audio.addEventListener("loadedmetadata", getTotalTime)
+
+      return () => audio.removeEventListener("loadedmetadata", getTotalTime)
+    }
+  }, [audio, isPlaying])
 
   return (
     <Container
@@ -46,6 +82,15 @@ const MediaPlayer = () => {
           <p style={{ fontSize: ".8rem" }} className=" text-secondary">
             {song?.artist?.name ?? ""}
           </p>
+          <Form.Range
+            min={0}
+            max={audio?.duration || 0}
+            value={valueRange}
+            onChange={(e) => (audioPlayer.current.currentTime = e.target.valueAsNumber)}
+          />
+          <p>
+            {songTime}/{songTimeTotal}
+          </p>
         </Col>
         <Col xs={3} className="p-0 d-flex justify-content-end gap-2 pe-3">
           {isPlaying ? (
@@ -69,7 +114,7 @@ const MediaPlayer = () => {
           )}
           <FontAwesomeIcon size="2xl" icon="fa-solid fa-forward" />
         </Col>
-        <audio id="audio" ref={audioPlayer} src={song?.preview} preload="metadata" />
+        <audio ref={audioPlayer} src={song?.preview} preload="metadata" />
       </Row>
     </Container>
   )
